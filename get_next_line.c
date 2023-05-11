50ea59b0cf47059e55c2e0a78a701bb0e2a0d7c3
Fiona Shane Salimbangon <fsalimba@F2N083.42adel.org.au>
2023-05-11 13:44:56 +0930

    follow 42 norminette standards

--- get_next_line.c
+++ get_next_line.c
@@ -1,96 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsalimba <fsalimba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 13:26:23 by fsalimba          #+#    #+#             */
+/*   Updated: 2023/05/11 13:32:57 by fsalimba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int ft_check_errors(int fd)
+int	ft_check_errors(int fd)
 {
-    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-        return (1);
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (1);
+	return (0);
+}
+
+int	ft_get_index(char *stash)
+{
+	int	i;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	return (i);
+}
+
+char	*ft_add_chars(int fd, char *stash)
+{
+	ssize_t	bytes_read;
+	char	buffer[BUFFER_SIZE + 1];
+
+	bytes_read = 0;
+	if (!stash)
+		stash = ft_strdup("");
+	while (!ft_strchr(stash, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (!bytes_read)
+			return (stash);
+		if (bytes_read < 0)
+			return (0);
+		buffer[bytes_read] = '\0';
+		stash = ft_strjoin(stash, buffer);
+		if (!stash)
+			return (0);
+	}
+	return (stash);
+}
+
+char	*ft_gen_line(char *stash)
+{
+	int		i;
+	char	*str;
+
+	if (!stash)
+		return (0);
+	i = ft_get_index(stash);
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*aux;
+	char		*line;
+	static char	*stash;
+
+	if (ft_check_errors(fd))
+	{
+		if (stash)
+			free(stash);
+		return (stash = 0);
+	}
+	stash = ft_add_chars(fd, stash);
+	if (stash == 0 || *stash == 0)
+	{
+		free(stash);
+		return (stash = 0);
+	}
+	line = ft_gen_line(stash);
+	aux = ft_strchr(stash, '\n');
+	if (aux && *aux)
+		aux = ft_strdup(aux + 1);
+	free(stash);
+	stash = aux;
+	return (line);
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+    int fd;
+    char *line;
+
+    fd = open("test.txt", O_RDONLY);
+    while ((line = get_next_line(fd)))
+    {
+        printf("%s\n", line);
+        free(line);
+    }
+    close(fd);
     return (0);
 }
-
-int ft_get_index(char *stash)
-{
-    int i;
-
-    i = 0;
-    while (stash[i] && stash[i] != '\n')
-        i++;
-    if (stash[i] == '\n')
-        i++;
-    return (i);
-}
-
-static char    *ft_add_chars(int fd, char *stash)
-{
-    ssize_t        bytes_read;
-    char        buffer[BUFFER_SIZE + 1];
-
-    bytes_read = 0;
-    if (!stash)
-        stash = ft_strdup("");
-    while (!ft_strchr(stash, '\n'))
-    {
-        bytes_read = read(fd, buffer, BUFFER_SIZE);
-        if (!bytes_read)
-            return (stash);
-        if (bytes_read < 0)
-            return (0);
-        buffer[bytes_read] = '\0';
-        stash = ft_strjoin(stash, buffer);
-        if (!stash)
-            return (0);
-    }
-    return (stash);
-}
-
-static char    *ft_gen_line(char *stash)
-{
-    int        i;
-    char    *s;
-
-    if (!stash)
-        return (0);
-    i = ft_get_index(stash);
-    s = (char *)malloc(sizeof(char) * (i + 1));
-    if (!s)
-        return (0);
-    i = 0;
-    while (stash[i] && stash[i] != '\n')
-    {
-        s[i] = stash[i];
-        i++;
-    }
-    if (stash[i] == '\n')
-    {
-        s[i] = stash[i];
-        i++;
-    }
-    s[i] = '\0';
-    return (s);
-}
-
-char    *get_next_line(int fd)
-{
-    char        *aux;
-    char        *line;
-    static char    *stash;
-
-    if (ft_check_errors(fd))
-    {
-        if (stash)
-            free(stash);
-        return (stash = 0);
-    }
-    stash = ft_add_chars(fd, stash);
-    if (stash == 0 || *stash == 0)
-    {
-        free(stash);
-        return (stash = 0);
-    }
-    line = ft_gen_line(stash);
-    aux = ft_strchr(stash, '\n');
-    if (aux && *aux)
-        aux = ft_strdup(aux + 1);
-    free(stash);
-    stash = aux;
-    return (line);
-}
+*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,65 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsalimba <fsalimba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 13:19:33 by fsalimba          #+#    #+#             */
+/*   Updated: 2023/05/11 13:25:55 by fsalimba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-size_t  ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char    *str;
-    size_t  size;
-    int     i;
-    int     j;
+	char	*str;
+	size_t	size;
+	int		i;
+	int		j;
 
-    size = ft_strlen(s1) + ft_strlen(s2) + 1;
-    str = (char *)malloc(size * sizeof(char));
-    if (!str)
-        return (0);
-    i = 0;
-    j = 0;
-    while (s1[j])
-        str[i++] = s1[j++];
-    j = 0;
-    while (s2[j])
-        str[i++] = s2[j++];
-    str[i] = '\0';
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = (char *)malloc(size * sizeof(char));
+	if (!str)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[j])
+		str[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
 	free(s1);
-    return (str);
+	return (str);
 }
 
-char    *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    while (*s)
-    {
-        if ((unsigned char)*s == (unsigned char)c)
-            return ((char *)s);
-        s++;
-    }
-    if (!c)
-        return ((char *)s);
-    return (0);
+	while (*s)
+	{
+		if ((unsigned char)*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (!c)
+		return ((char *)s);
+	return (0);
 }
 
-char    *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-    char    *dup;
-    int     i;
+	char	*dup;
+	int		i;
 
-    i = 0;
-    dup = (char *)malloc(ft_strlen(s) + 1);
-    if (!dup)
-        return (0);
-    while (s[i])
-    {
-        dup[i] = s[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
+	i = 0;
+	dup = (char *)malloc(ft_strlen(s) + 1);
+	if (!dup)
+		return (0);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
 }
